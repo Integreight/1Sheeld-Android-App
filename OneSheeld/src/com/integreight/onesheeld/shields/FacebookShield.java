3bd7043c3c23f13db334fd78b23861b4172663c2
Islam Mustafa <islam@integreight.com>
2013-09-30 14:56:22 +0200

    Finished the Facebook shield

--- OneSheeld/src/com/integreight/onesheeld/shields/FacebookShield.java
+++ OneSheeld/src/com/integreight/onesheeld/shields/FacebookShield.java
@@ -1,5 +1,12 @@
 package com.integreight.onesheeld.shields;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -7,8 +14,11 @@ import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 
+import com.facebook.FacebookRequestError;
+import com.facebook.HttpMethod;
 import com.facebook.LoggingBehavior;
 import com.facebook.Request;
+import com.facebook.RequestAsyncTask;
 import com.facebook.Response;
 import com.facebook.Session;
 import com.facebook.SessionState;
@@ -23,10 +33,14 @@ public class FacebookShield {
 	private String lastPost;
 	private Activity activity;
 	private Fragment fragment;
-	private static final byte FACEBOOK_COMMAND = (byte) 0x30;
+	private static final byte FACEBOOK_COMMAND = (byte) 0x31;
 	private static final byte UPDATE_STATUS_METHOD_ID = (byte) 0x01;
 
 	static final String PREF_KEY_FACEBOOK_USERNAME = "FacebookName";
+
+	private static final List<String> PERMISSIONS = Arrays
+			.asList("publish_actions");
+	private boolean pendingPublishReauthorization = false;
 
 	private static SharedPreferences mSharedPreferences;
 	private Session.StatusCallback statusCallback = new SessionStatusCallback();
@@ -45,17 +59,18 @@ public class FacebookShield {
 		Settings.addLoggingBehavior(LoggingBehavior.INCLUDE_ACCESS_TOKENS);
 
 		Session session = Session.getActiveSession();
-        if (session == null) {
-            if (savedInstanceState != null) {
-                session = Session.restoreSession(activity, null, statusCallback, savedInstanceState);
-            }
-            if (session == null) {
-                session = new Session(activity);
-            }
-            Session.setActiveSession(session);
-        }
-        
-        session.addCallback(statusCallback);
+		if (session == null) {
+			if (savedInstanceState != null) {
+				session = Session.restoreSession(activity, null,
+						statusCallback, savedInstanceState);
+			}
+			if (session == null) {
+				session = new Session(activity);
+			}
+			Session.setActiveSession(session);
+		}
+
+		session.addCallback(statusCallback);
 		this.fragment = fragment;
 	}
 
@@ -92,9 +107,9 @@ public class FacebookShield {
 				if (command == FACEBOOK_COMMAND) {
 					String post = new String(newArray);
 					lastPost = post;
-					// if (isTwitterLoggedInAlready())
-					// if(methodId==UPDATE_STATUS_METHOD_ID)
-					// new updateTwitterStatus().execute(tweet);
+					 if (isFacebookLoggedInAlready())
+					 if(methodId==UPDATE_STATUS_METHOD_ID)
+						 publishStory(post);
 
 				}
 
@@ -110,7 +125,9 @@ public class FacebookShield {
 
 	public static interface FacebookEventHandler {
 		void onRecievePost(String post);
+
 		void onFacebookLoggedIn();
+
 		void onFacebookError(String error);
 	}
 
@@ -146,30 +163,107 @@ public class FacebookShield {
 
 	private class SessionStatusCallback implements Session.StatusCallback {
 		@Override
-		public void call(Session session, SessionState state,
+		public void call(final Session session, SessionState state,
 				Exception exception) {
-			if(exception!=null&&eventHandler!=null)eventHandler.onFacebookError(exception.getMessage());
+			if (exception != null && eventHandler != null)
+				eventHandler.onFacebookError(exception.getMessage());
 			if (session.isOpened()) {
 
-				// make request to the /me API
-				Request.newMeRequest(session, new Request.GraphUserCallback() {
+				if (!pendingPublishReauthorization) {
+					List<String> permissions = session
+							.getPermissions();
+					if (!isSubsetOf(PERMISSIONS, permissions)) {
+						pendingPublishReauthorization = true;
+						Session.NewPermissionsRequest newPermissionsRequest = new Session.NewPermissionsRequest(
+								fragment, PERMISSIONS);
+						session.requestNewPublishPermissions(newPermissionsRequest);
 
-					// callback after Graph API response with user object
-					@Override
-					public void onCompleted(GraphUser user, Response response) {
-						if (user != null) {
-
-
-							Editor e = mSharedPreferences.edit();
-							e.putString(PREF_KEY_FACEBOOK_USERNAME, user.getName());
-							e.commit();
-							if(eventHandler!=null)eventHandler.onFacebookLoggedIn();
-						}
 					}
-				}).executeAsync();
+				}
+				
+				if(pendingPublishReauthorization&&state.equals(SessionState.OPENED_TOKEN_UPDATED)){
+					pendingPublishReauthorization = false;
+					Request.newMeRequest(session, new Request.GraphUserCallback() {
+
+						// callback after Graph API response with user object
+						@Override
+						public void onCompleted(GraphUser user, Response response) {
+							if (user != null) {
+									
+									Editor e = mSharedPreferences.edit();
+									e.putString(PREF_KEY_FACEBOOK_USERNAME,
+											user.getName());
+									e.commit();
+									if (eventHandler != null)
+										eventHandler.onFacebookLoggedIn();
+								}
+								
+							
+						}
+					}).executeAsync();
+				}
+				// make request to the /me API
+				
 
 			}
 		}
 	}
 
+	private boolean isSubsetOf(Collection<String> subset,
+			Collection<String> superset) {
+		for (String string : subset) {
+			if (!superset.contains(string)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private void publishStory(final String message) {
+		Session session = Session.getActiveSession();
+
+		if (session != null) {
+
+			Bundle postParams = new Bundle();
+			postParams.putString("message", message);
+//			postParams.putString("name", "Facebook SDK for Android");
+//			postParams.putString("caption",
+//					"Build great social apps and get more installs.");
+//			postParams
+//					.putString(
+//							"description",
+//							"The Facebook SDK for Android makes it easier and faster to develop Facebook integrated Android apps.");
+//			postParams.putString("link",
+//					"https://developers.facebook.com/android");
+//			postParams
+//					.putString("picture",
+//							"https://raw.github.com/fbsamples/ios-3.x-howtos/master/Images/iossdk_logo.png");
+
+			Request.Callback callback = new Request.Callback() {
+				public void onCompleted(Response response) {
+					JSONObject graphResponse = response.getGraphObject()
+							.getInnerJSONObject();
+					String postId = null;
+					try {
+						postId = graphResponse.getString("id");
+						if(eventHandler!=null)eventHandler.onRecievePost(message);
+					} catch (JSONException e) {
+						//Log.i("", "JSON error " + e.getMessage());
+						if(eventHandler!=null)eventHandler.onFacebookError(e.getMessage());
+					}
+					FacebookRequestError error = response.getError();
+					if (error != null) {
+						if(eventHandler!=null)eventHandler.onFacebookError(error.getErrorMessage());
+					}
+				}
+			};
+
+			Request request = new Request(session, "me/feed", postParams,
+					HttpMethod.POST, callback);
+
+			RequestAsyncTask task = new RequestAsyncTask(request);
+			task.execute();
+		}
+
+	}
 }
