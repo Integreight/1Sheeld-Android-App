bff6bc1908e837a8f866bd9f52afd12210bf8be4
Islam Mustafa <islam@integreight.com>
2013-10-14 15:06:05 +0200

    Added the fake shield of the musicplayer

--- OneSheeld/src/com/integreight/onesheeld/shieldsfragments/MusicPlayerFragment.java
+++ OneSheeld/src/com/integreight/onesheeld/shieldsfragments/MusicPlayerFragment.java
@@ -13,6 +13,6 @@ public class MusicPlayerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
             // Inflate the layout for this fragment
-            return inflater.inflate(R.layout.no_shield, container, false);
+            return inflater.inflate(R.layout.musicplayer_shield_fragment_layout, container, false);
         }
 }
