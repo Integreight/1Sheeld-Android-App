0d71c438cce2f621c5110a52733131a5d75719b0
Islam Mustafa <islam@integreight.com>
2014-02-17 18:39:49 +0200

    Made the sms shield work with the new frame

--- OneSheeld/src/com/integreight/onesheeld/shields/controller/SmsShield.java
+++ OneSheeld/src/com/integreight/onesheeld/shields/controller/SmsShield.java
@@ -4,15 +4,14 @@ import android.app.Activity;
 import android.telephony.SmsManager;
 
 import com.integreight.firmatabluetooth.ShieldFrame;
+import com.integreight.onesheeld.enums.UIShield;
 import com.integreight.onesheeld.utils.ControllerParent;
 
 public class SmsShield extends ControllerParent<SmsShield> {
 	private SmsEventHandler eventHandler;
 	private String lastSmsText;
 	private String lastSmsNumber;
-	private static final byte SMS_COMMAND = (byte) 0x36;
 	private static final byte SEND_SMS_METHOD_ID = (byte) 0x01;
-	private static final String separator = "##";
 
 	public String getLastSmsText() {
 		return lastSmsText;
@@ -37,25 +36,14 @@ public class SmsShield extends ControllerParent<SmsShield> {
 
 	@Override
 	public void onUartReceive(byte[] data) {
-		if (data.length < 2)
-			return;
-		byte command = data[0];
-		byte methodId = data[1];
-		int n = data.length - 2;
-		byte[] newArray = new byte[n];
-		System.arraycopy(data, 2, newArray, 0, n);
-		if (command == SMS_COMMAND) {
-			String smsRaw = new String(newArray);
-			String smsNumber = smsRaw.substring(0, smsRaw.indexOf(separator));
-			String smsText = smsRaw.substring(smsRaw.indexOf(separator)
-					+ separator.length());
-			lastSmsText = smsRaw;
-			if (methodId == SEND_SMS_METHOD_ID) {
-				sendSms(smsNumber, smsText);
+//		if (data.length < 2)
+//			return;
+//		byte command = data[0];
+//		byte methodId = data[1];
+//		int n = data.length - 2;
+//		byte[] newArray = new byte[n];
+//		System.arraycopy(data, 2, newArray, 0, n);
 
-			}
-
-		}
 		super.onUartReceive(data);
 	}
 
@@ -90,6 +78,16 @@ public class SmsShield extends ControllerParent<SmsShield> {
 	@Override
 	public void onNewShieldFrameReceived(ShieldFrame frame) {
 		// TODO Auto-generated method stub
+		if (frame.getShieldId() == UIShield.SMS_SHIELD.getId()) {
+			String smsNumber = frame.getArgumentAsString(0);
+			String smsText = frame.getArgumentAsString(1);
+			lastSmsText = smsText;
+			if (frame.getFunctionId() == SEND_SMS_METHOD_ID) {
+				sendSms(smsNumber, smsText);
+
+			}
+
+		}
 		
 	}
 
