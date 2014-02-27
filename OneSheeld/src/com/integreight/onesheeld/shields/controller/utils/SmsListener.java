5c35fe3249a0e572030b287eee15687d06eb9905
Ibrahim AbdelGawaad <iabdelgawaad@gyrolabs.tv>
2014-02-27 12:54:14 +0200

    SMS Sheeld Done

---
+++ OneSheeld/src/com/integreight/onesheeld/shields/controller/utils/SmsListener.java
@@ -0,0 +1,63 @@
+package com.integreight.onesheeld.shields.controller.utils;
+
+import com.integreight.onesheeld.shields.controller.utils.PhoneCallStateListener.PhoneRingingEventHandler;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+
+public class SmsListener extends BroadcastReceiver {
+	private SmsReceiveEventHandler eventHandler;
+
+	public void sendSMS()
+	{
+		eventHandler.onSmsReceiveSuccess("+201119611432","Welcome from OneSheeld, ya bash welcome to one sheed");
+	}
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// TODO Auto-generated method stub
+
+		if (intent.getAction()
+				.equals("android.provider.Telephony.SMS_RECEIVED")) {
+			Bundle bundle = intent.getExtras(); // ---get the SMS message passed
+												// in---
+			SmsMessage[] msgs = null;
+			String msg_from ="";
+			String msgBody = "";
+			if (bundle != null) {
+				// ---retrieve the SMS message received---
+				try {
+					Object[] pdus = (Object[]) bundle.get("pdus");
+					msgs = new SmsMessage[pdus.length];
+					for (int i = 0; i < msgs.length; i++) {
+						msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+						msg_from = msgs[i].getOriginatingAddress();
+						msgBody = msgs[i].getMessageBody();
+					}
+					// Send SMS to Arduino
+					if (!msg_from.isEmpty())
+					{
+						eventHandler.onSmsReceiveSuccess(msg_from,msgBody);
+					}
+				} catch (Exception e) {
+					// Fail to receive message
+					eventHandler.onSmsReceiveFailed();
+				}
+			}
+		}
+	}
+	public void setSmsReceiveEventHandler(
+			SmsReceiveEventHandler eventHandler) {
+		this.eventHandler = eventHandler;
+	}
+
+	public interface SmsReceiveEventHandler {
+		void onSmsReceiveSuccess(String mobile_number, String sms_body);
+
+		void onSmsReceiveFailed();
+
+	}
+
+}
