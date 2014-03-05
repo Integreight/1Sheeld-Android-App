c0de7e813f93533e99bdc0f1899438d827dbb35b
Ibrahim <iabdelgawaad@gyrolabs.net>
2014-03-05 12:53:03 +0200

    Add Email Sheeld need password

--- OneSheeld/src/com/integreight/onesheeld/shields/controller/EmailShield.java
+++ OneSheeld/src/com/integreight/onesheeld/shields/controller/EmailShield.java
@@ -1,15 +1,23 @@
 package com.integreight.onesheeld.shields.controller;
 
-import java.util.Calendar;
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
 
-import android.accounts.AccountManager;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 import android.app.Activity;
-import android.content.ActivityNotFoundException;
 import android.content.Intent;
-import android.provider.Settings;
+import android.os.AsyncTask;
 
-import com.google.android.gms.common.AccountPicker;
 import com.integreight.firmatabluetooth.ShieldFrame;
+import com.integreight.onesheeld.Log;
 import com.integreight.onesheeld.utils.ControllerParent;
 
 public class EmailShield extends ControllerParent<EmailShield> {
@@ -17,6 +25,9 @@ public class EmailShield extends ControllerParent<EmailShield> {
 	private EmailEventHandler eventHandler;
 	private static final byte EMAIL_COMMAND = (byte) 0x1E;
 	private static final byte SEND_METHOD_ID = (byte) 0x01;
+	private boolean isLoggedIn = false;
+	private String userEmail = "iabdelgawaad@gmail.com";
+	private String password = "********";
 
 	public EmailShield() {
 		super();
@@ -46,19 +57,109 @@ public class EmailShield extends ControllerParent<EmailShield> {
 	public void onNewShieldFrameReceived(ShieldFrame frame) {
 		// TODO Auto-generated method stub
 		if (frame.getShieldId() == EMAIL_COMMAND) {
-			//addGoogleAccount(getActivity());
+
+			if (isLoggedIn) {
+				// send Email
+				String email_send_to = frame.getArgumentAsString(0);
+				String subject = frame.getArgumentAsString(1);
+				String body = frame.getArgumentAsString(2);
+				eventHandler.onEmailsent(email_send_to, subject);
+				//sendMail(email_send_to, subject, body);
+				sendMailUsingJavaAPI(email_send_to, subject, body);
+			}
 		}
 
 	}
-	
-	public static void addGoogleAccount(final Activity activity) {
-	    final AccountManager accountMgr = AccountManager.get(activity);
-	    accountMgr.addAccount("com.google", "my_auth_token", null, null, activity, null, null);
+
+	public void setUserasLoggedIn(String userEmail , String password) {
+		isLoggedIn = true;
+		this.userEmail = userEmail;
+		this.password = password;
+	}
+
+	private void sendMail(String email, String subject, String body) {
+		Intent emailIntent = new Intent(Intent.ACTION_SEND);
+		emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[] { email });
+		emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
+		emailIntent.putExtra(Intent.EXTRA_TEXT, body);
+		emailIntent.setType("text/plain");
+		try {
+			getActivity().startActivity(
+					Intent.createChooser(emailIntent, "com.google").addFlags(
+							Intent.FLAG_ACTIVITY_NEW_TASK));
+		} catch (android.content.ActivityNotFoundException ex) {
+			Log.d("Email Sheeld:: Send Email()",
+					"There are no email clients installed");
+		}
+		Log.d("Email Sheeld:: Send Email()", email);
 	}
 
 	@Override
 	public void reset() {
 		// TODO Auto-generated method stub
 
+	}
+
+	private Session createSessionObject() {
+		Properties properties = new Properties();
+		properties.put("mail.smtp.auth", "true");
+		properties.put("mail.smtp.starttls.enable", "true");
+		properties.put("mail.smtp.host", "smtp.gmail.com");
+		properties.put("mail.smtp.port", "587");
+		return Session.getInstance(properties, new javax.mail.Authenticator() {
+		protected PasswordAuthentication getPasswordAuthentication() {
+		return new PasswordAuthentication(userEmail, password);}
+	
+		});
+		}
+
+	private Message createMessage(String email, String subject,
+			String messageBody, Session session) throws MessagingException,
+			UnsupportedEncodingException {
+		Message message = new MimeMessage(session);
+		message.setFrom(new InternetAddress("iabdelgawaad@gmail.com",
+				"Hema"));
+		message.addRecipient(Message.RecipientType.TO, new InternetAddress(
+				email));
+		message.setSubject(subject);
+		message.setText(messageBody);
+		return message;
+	}
+
+	private class SendMailTask extends AsyncTask<Message, Void, Void> {
+
+		@Override
+		protected void onPostExecute(Void aVoid) {
+			super.onPostExecute(aVoid);
+			Log.d("Email Sheeld:: SendMailTask ", "");
+
+		}
+
+		@Override
+		protected Void doInBackground(Message... messages) {
+			try {
+				Transport.send(messages[0]);
+			} catch (MessagingException e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
+	}
+
+	private void sendMailUsingJavaAPI(String email, String subject,
+			String messageBody) {
+		Session session = createSessionObject();
+
+		try {
+			Message message = createMessage(email, subject, messageBody,
+					session);
+			new SendMailTask().execute(message);
+		} catch (AddressException e) {
+			e.printStackTrace();
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
 	}
 }
--- OneSheeld/src/com/integreight/onesheeld/shields/fragments/EmailFragment.java
+++ OneSheeld/src/com/integreight/onesheeld/shields/fragments/EmailFragment.java
@@ -1,12 +1,16 @@
 package com.integreight.onesheeld.shields.fragments;
 
 import android.accounts.AccountManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
-import android.preference.PreferenceManager.OnActivityResultListener;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gms.common.AccountPicker;
@@ -19,9 +23,12 @@ import com.integreight.onesheeld.utils.ShieldFragmentParent;
 public class EmailFragment extends ShieldFragmentParent<EmailFragment> {
 
 	private static final int SOME_REQUEST_CODE = 0;
-	private static final int RESULT_OK = 0;
-	TextView sendTo, subject;
-	private boolean isAccountpickered = false;
+	TextView sendTo, subject , userName;
+	Button login_bt, logout_bt;
+	private static SharedPreferences mSharedPreferences;
+	private static final String PREF_EMAIL_SHIELD_USER_LOGIN = "user_login_status";
+	private static final String PREF_EMAIL_SHIELD_GMAIL_ACCOUNT = "gmail_account";
+	private String userEmail;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -54,26 +61,57 @@ public class EmailFragment extends ShieldFragmentParent<EmailFragment> {
 	public void onActivityCreated(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onActivityCreated(savedInstanceState);
+		((EmailShield) getApplication().getRunningShields().get(
+				getControllerTag()))
+				.setEmailEventHandler(emailEventHandler);
+	
 		Log.d("Email Sheeld::OnActivityCreated()", "");
 
 		sendTo = (TextView) getView().findViewById(
 				R.id.gmail_shield_sendto_textview);
+		userName = (TextView) getView().findViewById(R.id.gmail_shield_username_textview);
 		subject = (TextView) getView().findViewById(
 				R.id.gmail_shield_subject_textview);
-		
-
-		subject.setOnClickListener(new View.OnClickListener() {
-
+		login_bt = (Button) getView().findViewById(R.id.login_gmail_bt);
+		logout_bt = (Button) getView().findViewById(R.id.logout_gmail_bt);
+		login_bt.setOnClickListener(new View.OnClickListener() {
+			
 			@Override
 			public void onClick(View v) {
+				addAccount();
 			}
 		});
-		
-		Intent intent = AccountPicker.newChooseAccountIntent(null, null,
-				new String[] { "com.google" }, false, null, null, null, null);
-				intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-				getActivity().startActivityForResult(intent, SOME_REQUEST_CODE);
+		mSharedPreferences = getActivity().getApplicationContext()
+				.getSharedPreferences("com.integreight.onesheeld",
+						Context.MODE_PRIVATE);
 
+		// if user logged in run controller else ask for login
+		if (isGmailLoggedInAlready()) {
+			userEmail = mSharedPreferences.getString(PREF_EMAIL_SHIELD_GMAIL_ACCOUNT, "");
+			((EmailShield) getApplication().getRunningShields().get(
+					getControllerTag())).setUserasLoggedIn(userEmail,"");
+			userName.setVisibility(View.VISIBLE);
+			userName.setText(userEmail);
+			logout_bt.setVisibility(View.VISIBLE);
+		} else {
+			login_bt.setVisibility(View.VISIBLE);
+		}
+		
+		logout_bt.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				logoutGmailAccount();	
+
+			}
+		});
+
+	}
+
+	public boolean isGmailLoggedInAlready() {
+		// return twitter login status from Shared Preferences
+		return mSharedPreferences.getBoolean(PREF_EMAIL_SHIELD_USER_LOGIN,
+				false);
 	}
 
 	private EmailEventHandler emailEventHandler = new EmailEventHandler() {
@@ -100,37 +138,56 @@ public class EmailFragment extends ShieldFragmentParent<EmailFragment> {
 		if (getApplication().getRunningShields().get(getControllerTag()) == null) {
 			getApplication().getRunningShields().put(getControllerTag(),
 					new EmailShield(getActivity(), getControllerTag()));
-			((EmailShield) getApplication().getRunningShields().get(
-					getControllerTag()))
-					.setEmailEventHandler(emailEventHandler);
 		}
-
 	}
 
 	public void addAccount() {
 		Intent intent = AccountPicker.newChooseAccountIntent(null, null,
-		new String[] { "com.google" }, false, null, null, null, null);
-		intent.addFlags(Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT);
-		super.getAppActivity().startActivityForResult(intent, SOME_REQUEST_CODE);
+				new String[] { "com.google" }, false, null, null, null, null);
+		startActivityForResult(intent, SOME_REQUEST_CODE);
 	}
-	
 
 	public void onActivityResult(final int requestCode, final int resultCode,
 			final Intent data) {
-	//	super.onActivityResult(requestCode, resultCode, data);
 
-		if (requestCode == SOME_REQUEST_CODE && resultCode == RESULT_OK) {
-			if (isAccountpickered) {
-				String accountName = data
-						.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
-				Log.d("account name ", accountName);
-			}
-			isAccountpickered = true;
+		if (requestCode == SOME_REQUEST_CODE
+				&& resultCode == FragmentActivity.RESULT_OK) {
+			String accountName = data
+					.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+			Log.d("account name ", accountName);
+			// save in share perefrences
+			SharedPreferences.Editor editor = mSharedPreferences.edit();
+			editor.putString(PREF_EMAIL_SHIELD_GMAIL_ACCOUNT, accountName);
+			editor.putBoolean(PREF_EMAIL_SHIELD_USER_LOGIN, true);
+
+			// Commit the edits!
+			editor.commit();
+			((EmailShield) getApplication().getRunningShields().get(
+					getControllerTag())).setUserasLoggedIn(accountName,"");
+			login_bt.setVisibility(View.INVISIBLE);
+			logout_bt.setVisibility(View.VISIBLE);
+			userName.setVisibility(View.VISIBLE);
+			userName.setText(accountName);
 		}
+		super.onActivityResult(requestCode, resultCode, data);
 	}
 
 	@Override
 	public void doOnServiceConnected() {
 		initializeFirmata();
+	}
+	
+	private void logoutGmailAccount() {
+		Editor e = mSharedPreferences.edit();
+		e.remove(PREF_EMAIL_SHIELD_GMAIL_ACCOUNT);
+		e.remove(PREF_EMAIL_SHIELD_USER_LOGIN);
+		e.commit();
+		login_bt.setVisibility(View.VISIBLE);
+		logout_bt.setVisibility(View.INVISIBLE);
+		userName.setVisibility(View.INVISIBLE);
+		subject.setText("");
+		sendTo.setText("");
+
+
 	}
 }
