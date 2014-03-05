0d42bedcf71d160d76b08c761671707c6b2504f5
Ibrahim <iabdelgawaad@gyrolabs.net>
2014-03-05 13:43:22 +0200

    delete java mail API

--- OneSheeld/src/com/integreight/onesheeld/shields/controller/EmailShield.java
+++ OneSheeld/src/com/integreight/onesheeld/shields/controller/EmailShield.java
@@ -1,20 +1,7 @@
 package com.integreight.onesheeld.shields.controller;
 
-import java.io.UnsupportedEncodingException;
-import java.util.Properties;
-
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-
 import android.app.Activity;
 import android.content.Intent;
-import android.os.AsyncTask;
 
 import com.integreight.firmatabluetooth.ShieldFrame;
 import com.integreight.onesheeld.Log;
@@ -26,8 +13,6 @@ public class EmailShield extends ControllerParent<EmailShield> {
 	private static final byte EMAIL_COMMAND = (byte) 0x1E;
 	private static final byte SEND_METHOD_ID = (byte) 0x01;
 	private boolean isLoggedIn = false;
-	private String userEmail = "iabdelgawaad@gmail.com";
-	private String password = "********";
 
 	public EmailShield() {
 		super();
@@ -64,17 +49,14 @@ public class EmailShield extends ControllerParent<EmailShield> {
 				String subject = frame.getArgumentAsString(1);
 				String body = frame.getArgumentAsString(2);
 				eventHandler.onEmailsent(email_send_to, subject);
-				//sendMail(email_send_to, subject, body);
-				sendMailUsingJavaAPI(email_send_to, subject, body);
+				// sendMail(email_send_to, subject, body);
 			}
 		}
 
 	}
 
-	public void setUserasLoggedIn(String userEmail , String password) {
+	public void setUserasLoggedIn(String userEmail, String password) {
 		isLoggedIn = true;
-		this.userEmail = userEmail;
-		this.password = password;
 	}
 
 	private void sendMail(String email, String subject, String body) {
@@ -100,66 +82,4 @@ public class EmailShield extends ControllerParent<EmailShield> {
 
 	}
 
-	private Session createSessionObject() {
-		Properties properties = new Properties();
-		properties.put("mail.smtp.auth", "true");
-		properties.put("mail.smtp.starttls.enable", "true");
-		properties.put("mail.smtp.host", "smtp.gmail.com");
-		properties.put("mail.smtp.port", "587");
-		return Session.getInstance(properties, new javax.mail.Authenticator() {
-		protected PasswordAuthentication getPasswordAuthentication() {
-		return new PasswordAuthentication(userEmail, password);}
-	
-		});
-		}
-
-	private Message createMessage(String email, String subject,
-			String messageBody, Session session) throws MessagingException,
-			UnsupportedEncodingException {
-		Message message = new MimeMessage(session);
-		message.setFrom(new InternetAddress("iabdelgawaad@gmail.com",
-				"Hema"));
-		message.addRecipient(Message.RecipientType.TO, new InternetAddress(
-				email));
-		message.setSubject(subject);
-		message.setText(messageBody);
-		return message;
-	}
-
-	private class SendMailTask extends AsyncTask<Message, Void, Void> {
-
-		@Override
-		protected void onPostExecute(Void aVoid) {
-			super.onPostExecute(aVoid);
-			Log.d("Email Sheeld:: SendMailTask ", "");
-
-		}
-
-		@Override
-		protected Void doInBackground(Message... messages) {
-			try {
-				Transport.send(messages[0]);
-			} catch (MessagingException e) {
-				e.printStackTrace();
-			}
-			return null;
-		}
-	}
-
-	private void sendMailUsingJavaAPI(String email, String subject,
-			String messageBody) {
-		Session session = createSessionObject();
-
-		try {
-			Message message = createMessage(email, subject, messageBody,
-					session);
-			new SendMailTask().execute(message);
-		} catch (AddressException e) {
-			e.printStackTrace();
-		} catch (MessagingException e) {
-			e.printStackTrace();
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
-	}
 }
