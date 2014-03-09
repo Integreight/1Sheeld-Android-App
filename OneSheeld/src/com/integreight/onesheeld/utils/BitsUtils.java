9803c5a232033955e0e369954a12b2087f9e5682
Islam Mustafa <islam@integreight.com>
2014-03-09 13:59:06 +0200

    Fixed an error with the last commit

--- OneSheeld/src/com/integreight/onesheeld/utils/BitsUtils.java
+++ OneSheeld/src/com/integreight/onesheeld/utils/BitsUtils.java
@@ -4,11 +4,11 @@ public class BitsUtils {
 
 	public static byte setBit(byte b,int bit){
 		if (bit>8)return b;
-		return (byte)(b|(b<<bit));
+		return (byte)(b|(1<<bit));
 	}
 	
 	public static byte resetBit(byte b,int bit){
 		if (bit>8)return b;
-		return (byte)(b&(~(b<<bit)));
+		return (byte)(b&(~(1<<bit)));
 	}
 }
