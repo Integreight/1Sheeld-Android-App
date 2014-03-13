5c74e43fee3a3ba02f1257d25c8e287352e497ef
Ibrahim <iabdelgawaad@gyrolabs.net>
2014-03-13 19:03:20 +0200

    Fixed buzz on/off in Buzzer sheeld

--- OneSheeld/src/com/integreight/onesheeld/shields/controller/SpeakerShield.java
+++ OneSheeld/src/com/integreight/onesheeld/shields/controller/SpeakerShield.java
@@ -19,8 +19,7 @@ public class SpeakerShield extends ControllerParent<ControllerParent<?>> {
 	private static final byte BUZZER_OFF = (byte) 0x00;
 	private boolean isResumed = false;
 	MediaPlayer mp;
-	private static final int soundResourceId=R.raw.door_chime_sound;
-
+	private static final int soundResourceId = R.raw.door_chime_sound;
 
 	public SpeakerShield() {
 		super();
@@ -43,17 +42,18 @@ public class SpeakerShield extends ControllerParent<ControllerParent<?>> {
 	public void onNewShieldFrameReceived(ShieldFrame frame) {
 
 		if (frame.getShieldId() == BUZZER_COMMAND) {
-			switch (frame.getFunctionId()) {
+			byte argumentValue = frame.getArgument(0)[0];
+			switch (argumentValue) {
 			case BUZZER_ON:
 				// turn on bin
 				playSound(soundResourceId);
-				if(isResumed)
-				eventHandler.onSpeakerChange(true);
+				if (isResumed)
+					eventHandler.onSpeakerChange(true);
 				break;
 			case BUZZER_OFF:
 				// turn off bin
-				if(isResumed)
-				eventHandler.onSpeakerChange(false);
+				if (isResumed)
+					eventHandler.onSpeakerChange(false);
 				break;
 			default:
 				break;
@@ -62,37 +62,28 @@ public class SpeakerShield extends ControllerParent<ControllerParent<?>> {
 		}
 	}
 
-	public void doOnResume ()
-	{
+	public void doOnResume() {
 		isResumed = true;
 	}
-	public void playSound(int soundResourceId){
-		if(mp==null)
+
+	public void playSound(int soundResourceId) {
+		if (mp == null)
 			mp = MediaPlayer.create(getApplication(), soundResourceId);
-		/*if(mp.isPlaying()){
-			Resources res = getActivity().getResources();
-			AssetFileDescriptor afd = res.openRawResourceFd(soundResourceId);
-			FileDescriptor fd = afd.getFileDescriptor();
-			mp.reset();
-			try {
-				mp.setDataSource(fd);
-				mp.prepare();
-			} catch (IllegalArgumentException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IllegalStateException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			mp.start();
-		}
-		else*/
-			mp.start();
+		/*
+		 * if(mp.isPlaying()){ Resources res = getActivity().getResources();
+		 * AssetFileDescriptor afd = res.openRawResourceFd(soundResourceId);
+		 * FileDescriptor fd = afd.getFileDescriptor(); mp.reset(); try {
+		 * mp.setDataSource(fd); mp.prepare(); } catch (IllegalArgumentException
+		 * e) { // TODO Auto-generated catch block e.printStackTrace(); } catch
+		 * (IllegalStateException e) { // TODO Auto-generated catch block
+		 * e.printStackTrace(); } catch (IOException e) { // TODO Auto-generated
+		 * catch block e.printStackTrace(); }
+		 * 
+		 * mp.start(); } else
+		 */
+		mp.start();
 	}
+
 	@Override
 	public void reset() {
 		// TODO Auto-generated method stub
