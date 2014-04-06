22408872fd15e32b095df3e7b78ce8632ad20b8a
Islam Mustafa <islam@integreight.com>
2014-04-06 12:27:12 +0200

    implemented firmata version as native

--- OneSheeld/src/com/integreight/firmatabluetooth/ArduinoFirmata.java
+++ OneSheeld/src/com/integreight/firmatabluetooth/ArduinoFirmata.java
@@ -57,7 +57,7 @@ public class ArduinoFirmata {
 	private final byte RESET_MICRO = (byte) 0x60;
 	private final byte BLUETOOTH_RESET = (byte) 0x61;
 	// private final byte IS_ALIVE = (byte) 0x62;
-	private final byte FIRMWARE_VERSION_QUERY = (byte) 0x63;
+	//private final byte FIRMWARE_VERSION_QUERY = (byte) 0x63; //Deprecated
 	private final byte MUTE_FIRMATA = (byte) 0x64;
 	private final byte UART_COMMAND = (byte) 0x65;
 	private final byte UART_DATA = (byte) 0x66;
@@ -362,15 +362,15 @@ public class ArduinoFirmata {
 							}
 						}
 						
-						if (sysexCommand == FIRMWARE_VERSION_QUERY) {
-							if (sysexData.length >= 2) {
-								setVersion(sysexData[1], sysexData[0]);
-								for (ArduinoVersionQueryHandler handler : versionQueryHandlers) {
-									handler.onVersionReceived(minorVersion,
-											majorVersion);
-								}
-							}
-						}
+//						if (sysexCommand == FIRMWARE_VERSION_QUERY) {
+//							if (sysexData.length >= 2) {
+//								setVersion(sysexData[1], sysexData[0]);
+//								for (ArduinoVersionQueryHandler handler : versionQueryHandlers) {
+//									handler.onVersionReceived(minorVersion,
+//											majorVersion);
+//								}
+//							}
+//						}
 						if (sysexCommand == BLUETOOTH_RESET) {
 							if(!isBootloader){
 							sysex(BLUETOOTH_RESET, new byte[]{});
@@ -489,7 +489,7 @@ public class ArduinoFirmata {
 	}
 
 	private void queryVersion() {
-		sysex(FIRMWARE_VERSION_QUERY, new byte[] {});
+		write(REPORT_VERSION);
 	}
 
 	private void clearAllBuffers() {
