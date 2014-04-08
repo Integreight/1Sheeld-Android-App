236127678633a3aa63cc3067d339322784806631
Islam Mustafa <islam@integreight.com>
2014-04-08 22:07:07 +0200

    Removed unused imports

--- OneSheeld/src/com/integreight/firmatabluetooth/BluetoothService.java
+++ OneSheeld/src/com/integreight/firmatabluetooth/BluetoothService.java
@@ -19,7 +19,6 @@ package com.integreight.firmatabluetooth;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.ArrayList;
 import java.util.UUID;
 import java.util.concurrent.CopyOnWriteArrayList;
 
