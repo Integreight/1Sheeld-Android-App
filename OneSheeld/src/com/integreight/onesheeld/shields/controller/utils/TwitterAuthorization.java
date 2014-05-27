f52921a8e69bf49596c8c324d43bfc87c616ce46
Islam Mustafa <islam@integreight.com>
2014-05-27 05:24:54 +0200

    Changed twitter application

--- OneSheeld/src/com/integreight/onesheeld/shields/controller/utils/TwitterAuthorization.java
+++ OneSheeld/src/com/integreight/onesheeld/shields/controller/utils/TwitterAuthorization.java
@@ -2,9 +2,9 @@ package com.integreight.onesheeld.shields.controller.utils;
 
 public class TwitterAuthorization {
 
-	public static String CONSUMER_KEY = "W89WcoS0fjGJ4pGgyBmA";
+	public static String CONSUMER_KEY = "RUALr0EwkNRJUUnnRS6DzwLs1";
 
-	public static String CONSUMER_SECRET = "izyhv5RIIaQgNsYX55hHVMj0BYm2p5bWQk8vqlasTo";
+	public static String CONSUMER_SECRET = "y9r7afp7hxVhpMLhyc1eIQcifZv5hxFFXQYOuZIXw1onSIGsC3";
 
 	public static String REQUEST_TOKEN_URL = "https://api.twitter.com/oauth/request_token";
 
