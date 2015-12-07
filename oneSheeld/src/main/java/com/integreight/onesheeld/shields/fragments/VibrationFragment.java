f65121e989c85c2e5b02c8074aae209c442d1c3a
Mostafa Mahmoud <Mostafa@Mostafa-Mahmoud.local>
2015-12-07 06:24:21 +0200

    Add a button to stop vibrations manually in Vibration Shield
    Changed layout of Vibration Shield

--- oneSheeld/src/main/java/com/integreight/onesheeld/shields/fragments/VibrationFragment.java
+++ oneSheeld/src/main/java/com/integreight/onesheeld/shields/fragments/VibrationFragment.java
@@ -5,19 +5,39 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.integreight.onesheeld.R;
 import com.integreight.onesheeld.shields.ShieldFragmentParent;
 import com.integreight.onesheeld.shields.controller.VibrationShield;
 
 /**
- * Created by Mostafa on 11/22/15.
+ * Author: Mostafa Mahmoud
+ * Email: mostafa_mahmoud@protonmail.com
+ * Created on: 11/22/15
  */
 public class VibrationFragment extends ShieldFragmentParent<VibrationFragment>{
+    private Button vibrationStopButton;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.vibration_shield_fragment_layout,container,false);
+    }
+
+    @Override
+    public void doOnViewCreated(View view, Bundle savedInstanceStat) {
+        vibrationStopButton = (Button) view.findViewById(R.id.vibration_stop_button);
+    }
+
+    @Override
+    public void doOnStart() {
+        vibrationStopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((VibrationShield) getApplication().getRunningShields().get(getControllerTag()))
+                        .stop();
+            }
+        });
     }
 
     @Override
